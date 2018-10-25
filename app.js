b49c7206fa839bc5b7c22ec15dbd106323e28a08
Gian Gonzaga <giangonzaga29@gmail.com>
2018-10-25 15:04:26 +0800

    Finished lesson

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-new Vue({
+let app = new Vue({
     el: "#vue-app",
     data: {
-
+        name: '',
+        age: ''
     },
     methods: {
         logName: function() {
-            console.log('You entered your name')
         },
         logAge: function() {
             console.log('You entered your age')
